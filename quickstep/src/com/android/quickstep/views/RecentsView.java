c279a3d183bb853699b24cc912985e5812459192
Pranav <npv12@iitbbs.ac.in>
2022-03-21 09:58:28 +0530

    launcher3: Don't vibrate while scrolling
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -1904,14 +1904,7 @@ public abstract class RecentsView<
         vibrateForScroll();
     }
 
-    private void vibrateForScroll() {
-        long now = SystemClock.uptimeMillis();
-        if (now - mScrollLastHapticTimestamp > mScrollHapticMinGapMillis) {
-            mScrollLastHapticTimestamp = now;
-            VibratorWrapper.INSTANCE.get(mContext).vibrate(SCROLL_VIBRATION_PRIMITIVE,
-                    SCROLL_VIBRATION_PRIMITIVE_SCALE, SCROLL_VIBRATION_FALLBACK);
-        }
-    }
+    private void vibrateForScroll() {}
 
     @Override
     protected void determineScrollingStart(MotionEvent ev, float touchSlopScale) {
